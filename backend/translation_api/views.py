ea7daaab74f346923d9b9be5c4f9d52853392129
NiceAndFlowy <NiceAndFlowy@user.noreply.github.com>
2017-11-01 18:54:05 -0400

    Added serverside sanitizing using bleach

--- backend/translation_api/views.py
+++ backend/translation_api/views.py
@@ -6,20 +6,35 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from . models import Translation
-from . serializers import TranslationSerializer
+from langdetect import detect
+from translate import Translator
+from mtranslate import translate
+import bleach
+import json
+from .models import Translation
+from .serializers import TranslationSerializer
+
 
 class TranslationList(APIView):
-
     def get(self, request):
         translations = Translation.objects.all()
         serializer = TranslationSerializer(translations, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        print(request.data)
-        serializer = TranslationSerializer(data=request.data)
+        try:
+            inputText = request.data['inputText']
+            detectedLanguage = detect(inputText)
+            bleachedInputText = bleach.clean(inputText)
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        to_lang = 'en'
+        translatedText = translate(bleachedInputText, to_lang, detectedLanguage)
+        print('detected language:%s; input:%s; translatedText:%s' %(detectedLanguage, bleachedInputText, translatedText))
+
+        data = {'inputText': bleachedInputText, 'detectedLanguage': detectedLanguage, 'translatedText': translatedText}
+        serializer = TranslationSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
